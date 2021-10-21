5d2daf0228d427f48da115f2fb17797e924c1468
kjhmh2 <1492012973@qq.com>
2021-10-21 10:55:46 +0100

    update my_group structure

--- group.py
+++ group.py
@@ -2,4 +2,35 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    'Jill':
+    {
+        'age': 26,
+        'job': 'biologist',
+        'connection': {'Zalika': 'friend', 'John': 'partner'},
+    },
+    'Zalika':
+    {
+        'age': 28,
+        'job': 'artist',
+        'connection': {'Jill': 'friend'},
+    },
+    'John':
+    {
+        'age': 27,
+        'job': 'writer',
+        'connection': {'Jill': 'partner'},
+    },
+    'Nash':
+    {
+        'age': 34,
+        'job': 'chef',
+        'connection': {'John': 'cousin', 'Zalika': 'landlord'},
+    }
+}
+
+# Questions:
+# 1 Yes
+# 2 Yes
+# 3 No
+
